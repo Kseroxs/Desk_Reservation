0121c32b05bba0301f6e19f243704dd134d662b7
Szymon <erbopl@gmail.com>
2022-12-20 19:08:06 +0100

    Registration Panel

--- Registration.aspx.cs
+++ Registration.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,22 @@ namespace Desk_Reservation
 {
     public partial class Registration : System.Web.UI.Page
     {
+        SqlConnection myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["DeskReservationConnectionString"].ConnectionString);
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void RegistrationButton_Click(object sender, EventArgs e)
+        {
+            myConnection.Open();
+            string query = "insert into [dbo].[Users]([Login],[Password],[Email]) Values(@login,@password,@email)";
+            SqlCommand insertCommand = new SqlCommand(query, myConnection);
+            insertCommand.Parameters.AddWithValue("@login", LoginTextBox.Text);
+            insertCommand.Parameters.AddWithValue("@password", PasswordTextBox.Text);
+            insertCommand.Parameters.AddWithValue("@email", EmailTextBox.Text);
+            insertCommand.ExecuteNonQuery();
+            myConnection.Close();
         }
     }
 }
